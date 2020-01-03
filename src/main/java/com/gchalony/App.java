e1d9db0108711d968fb9005828fea984a3dd9279
GregDeLaMort <gchalony@yahoo.fr>
2020-01-03 17:03:17 +0100

    Playing around

--- src/main/java/com/gchalony/App.java
+++ src/main/java/com/gchalony/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Message msg = new Message();
+        System.out.println( "Hello World, here is my message: " + msg.content);
     }
 }
---
+++ src/main/java/com/gchalony/Message.java
@@ -0,0 +1,14 @@
+package com.gchalony;
+
+public class Message {
+	public String content;
+	
+	public Message(String msg) {
+		content = msg;
+	}
+	
+	public Message() {
+		content = "Maven is great!";
+	}
+
+}
